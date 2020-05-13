dbab0e8ab58e77e798019b9ab9eea4c5b12a16ec
unknown <jdogisme@hotmail.comm>
2020-05-13 13:14:23 -0400

    Updates

---
+++ precache-manifest.e72ae5fbac0e2578653d94164f26e796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca339a8d62c241bade3146d176ec0f6c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "adf80234bb007432cf59",
+    "url": "/monsters-rolodex/static/css/main.a5aecff3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf80234bb007432cf59",
+    "url": "/monsters-rolodex/static/js/main.7aa26f39.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
